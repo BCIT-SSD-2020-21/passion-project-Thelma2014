437b50f724f654b871331a2afa7e77e8b97d99d5
Thelma Palma <design@thelmapalma.com>
2021-04-14 16:50:11 -0700

    added Signup.css

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import Form from "react-boostrap/From";
 import { useHistory } from "react-router-dom";
 
+import { useAppContext } from "../libs/contextLib";
+import { useFormFields } from "../libs/hooksLib";
+import "../style/containers/Signup.css";
+
 
 
 export default function Signup() {
