ab7f36b8dd15bd72f5f233927f6d33c06aa81718
Thelma Palma <design@thelmapalma.com>
2021-04-14 12:13:49 -0700

    Adding Form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import { AmplifyPasswordField } from '@aws-amplify/ui-react';
 import React, { useState } from 'react';
 import From from "react-boostrap/Form";
+import { Form } from 'react-bootstrap';
 import Buttton from "react-bootstrap/Button";
 import "./Login.css";
 
@@ -20,7 +21,28 @@ export default function Login() {
 
     return (
         <div className="Login">
-            
+            <Form onSubmit={handleSubmit}>
+                <Form.Group size="lg" controlId="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        autoFocus
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        />
+                </Form.Group>
+                <Form.Group size="lg" controlId="password">
+                    <From.Label>Password</From.Label>
+                    <Form.Control
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.vlue)}
+                    />
+                </Form.Group>
+                <Button block size="lg" type="submit" disable={!validateForm()}>
+                    Login
+                </Button>
+            </Form>            
         </div>
     );
 }
