275b3c6f3ff6edfe16a7483e3ad014d36a7f7b84
Thelma Palma <design@thelmapalma.com>
2021-04-14 17:09:56 -0700

    Load user session

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import Routes from './Routes';
 import Nav from "react-bootstrap/Nav";
+import { Auth } from "aws-amplify";
 import './App.css';
 
 
 function App() {
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+  useEffect(() => {
+    onLoad(); 
+  }, []);
+
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true)
+    }
+    catch(e) {
+      if (e !== "No current user") {
+        alert(e);
+      }
+    }
+
+    setIsAuthenticating(false);
+
+  }
+
+
   return (
     <div className="App">
       <Routes />
