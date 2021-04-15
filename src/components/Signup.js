02c5f34556ec0bdf41e9cdb96a959458a0f7d671
Thelma Palma <design@thelmapalma.com>
2021-04-15 12:53:38 -0700

    re-useFormFields custom React Hook

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,14 +1,30 @@
 import React, { useState } from 'react';
 import Form from "react-boostrap/From";
 import { useHistory } from "react-router-dom";
-
+import LoadButton from "../components/LoaderButton";
 import { useAppContext } from "../libs/contextLib";
 import { useFormFields } from "../libs/hooksLib";
+import { onError } from "../libs/errorLib";
 import "../style/containers/Signup.css";
 
 
 
 export default function Signup() {
+    const [fields, handleFieldChange] = useFormFields({
+        email:"",
+        password: "",
+        confirmPassword: "",
+        confirmationCode: "",
+    });
+
+    const history = useHistory();
+    const [newUser, setNewUser] =useState(null);
+    const { userHAsAuthenticated } = useAppContext();
+    const [isLoading, setIsLoading] = useState(false);
+
+
+
+
     return (
         <div>
             
