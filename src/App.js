ac21d0505b1aebb6435054a8264f719034b212b8
Thelma Palma <design@thelmapalma.com>
2021-04-14 17:25:38 -0700

    Added Navbar/ integrate Auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Routes from './Routes';
 import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
+import { useAppContext } from "./libs/contextLib";
 import { Auth } from "aws-amplify";
 import './App.css';
 
@@ -30,10 +33,38 @@ function App() {
 
 
   return (
-    <div className="App">
-      <Routes />
-    </div>
+    !isAuthenticating && (
+      <div className="App container py-3">
+        <Navbar collapseOnSelect expand="md" className="mb-3">
+          <LinkContainer to="/">
+            <Navbar.Brand className="font-weight-bold text-muted">
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav activeKey={window.location.pathname}>
+              {isAuthenticated ? (
+                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+              ) : (
+                <>
+                  <LinkContainer to="/signup">
+                    <Nav.Link>Signup</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <Nav.Link>Login</Nav.Link>
+                  </LinkContainer>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+          <Routes />
+        </AppContext.Provider>
+      </div>
+    )
   );
+
 }
 
 export default App;
