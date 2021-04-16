5a851ac9f8552cd51c93e01dd184c64232fa1554
Thelma Palma <design@thelmapalma.com>
2021-04-16 15:20:07 -0700

    installed Trix dependency

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react'
 import "../style/containers/Home.css";
-
-export default function Home() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
 }
