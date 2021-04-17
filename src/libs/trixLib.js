08b3179ca1d4f5578bb6fa9a5066b09dfb4a24ce
Thelma Palma <design@thelmapalma.com>
2021-04-17 09:40:16 -0700

    aded create video Button Youtube

--- src/libs/trixLib.js
+++ src/libs/trixLib.js
@@ -1,6 +1,66 @@
 import trix from 'trix';
 import { s3Upload } from "../libs/awsLib";
 
+export async function createVideoButton(editor) {
+
+    var videoButton = document.createElement("button");
+    videoButton.setAttribute("type", "button");
+    videoButton.setAttribute("class", "trix-button trix-button--icon trix-button--icon-video");
+    videoButton.setAttribute("data-trix-attribute", "video");
+    videoButton.setAttribute("title", "Embed Video");
+    videoButton.setAttribute("tabindex", "-1");
+    videoButton.innerText = "Video";
+  
+    document.querySelector(".trix-button-group.trix-button-group--block-tools").appendChild(videoButton);
+  
+    var videoDialog =
+    `<div class="trix-dialog trix-dialog--video" data-trix-dialog="video" data-trix-dialog-attribute="video">
+      <div class="trix-dialog__link-fields">
+        <input type="url" name="href" class="trix-input trix-input--dialog" placeholder="Enter a Youtube URL…" aria-label="URL" required="" data-trix-input="" disabled="disabled">
+        <div class="trix-button-group">
+          <input type="button" class="trix-button trix-button--dialog" value="Insert" data-trix-dialog-submit--video>
+        </div>
+      </div>
+    </div>`;
+    document.querySelector("[data-trix-dialogs]").insertAdjacentHTML("beforeend", videoDialog);
+  
+    document.querySelector("[data-trix-dialog-submit--video]").addEventListener('click', function() {
+      var videoElement = document.querySelector("[data-trix-dialog=video] input[name=href]");
+      
+      if (videoElement.value) {
+        
+        var youtube_id = '';
+        var url = videoElement.value.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+        if(url[2] !== undefined) {
+          youtube_id = url[2].split(/[^0-9a-z_\-]/i);
+          youtube_id = youtube_id[0];
+        }
+        else {
+          youtube_id = url;
+        }
+  
+  
+        var videoThumb = '<img src="https://img.youtube.com/vi/'+youtube_id+'/0.jpg">'
+        var attachmentThumb = new trix.Attachment({
+          contentType: 'application/youtube-thumb.html',
+          content: videoThumb
+        })
+        editor.insertAttachment(attachmentThumb);
+  
+        var videoEmbed = '<iframe width="420" height="315" src="https://www.youtube.com/embed/'+youtube_id+'" frameborder="0" allowfullscreen></iframe>'
+        var attachmentVideo = new trix.Attachment({
+          contentType: 'application/youtube-video.html',
+          content: videoEmbed
+        })
+        editor.insertAttachment(attachmentVideo);
+  
+        editor.selectionManager.unlock();
+  
+        videoElement.value = "";
+      }
+    })
+  }
+
 export async function createUploadButton(editor) {
     var button = document.createElement("button");
     button.setAttribute("type", "button");
